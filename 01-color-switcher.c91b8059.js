462d72e7eafc8f3a869e6e04dfc2ce48c6545f83
OleksanderMelnik <131677126+OleksanderMelnik@users.noreply.github.com>
2023-09-13 09:23:34 +0000

    Deploying to gh-pages from @ OleksanderMelnik/goit-js-hw-09@270388a410ef3a3cc5747a7e872401070f90ba3a 🚀

---
+++ 01-color-switcher.c91b8059.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let d=null;e.setAttribute("disabled",!0),t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){e.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.c91b8059.js.map
